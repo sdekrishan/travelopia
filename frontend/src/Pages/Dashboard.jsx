938114c8ea6a09af6c2cb49e28b291f929e77895
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-05-27 19:26:33 +0530

    fixes : some ui corrections added button in dashboard page for navigate

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,88 +1,130 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/layout';
-import axios from 'axios'
-import React, { useEffect, useRef, useState } from 'react'
-import Loader from '../Components/Loader';
-import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/table';
-import { Button } from '@chakra-ui/button';
-import {AiOutlineReload} from "react-icons/ai"
+import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import Loader from "../Components/Loader";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
+import { Button } from "@chakra-ui/button";
+import { AiOutlineReload } from "react-icons/ai";
+import { FaWpforms } from "react-icons/fa";
+import { useNavigate } from "react-router";
 
 const Dashboard = () => {
-    const [currentPage ,setCurrentPage] = useState(1)
-    const [bookings, setBookings] = useState([]);
-    const [loading ,setLoading] = useState(false);
-    const [totalBooking, setTotalBookings] = useState(0)
-    
-    useEffect(()=>{
-        setLoading(true)
-        axios.get(`https://travelopia-3psd.onrender.com/booking?page=1`)
-        .then(res => {
-            console.log(res);
-            setBookings(res.data.allBookings)
-            setTotalBookings(res.data.total)
-        })
-        .catch(err => console.log(err));
-        setLoading(false)
-    },[]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalBooking, setTotalBookings] = useState(0);
+  const navigate = useNavigate();
 
-    
-    
-    const getPagePosts = (p) => {
-        setCurrentPage(p)
-        setLoading(true)
-        axios.get(`https://travelopia-3psd.onrender.com/booking?page=${p}`)
-        .then(res => {
-            setLoading(false)
-            setBookings(res.data.allBookings)
-        })
-        .catch(err => setLoading(false));
-        
-    }
-    const downloadCSV =()=> {
-        
-            fetch(`https://travelopia-3psd.onrender.com/booking?page=${currentPage}`) // Replace with your API endpoint
-              .then(response => response.text())
-              .then(csvData => {
-                const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8' });
-                const link = document.createElement('a');
-                link.href = URL.createObjectURL(blob);
-                link.download = 'data.csv';
-                link.click();
-              })
-              .catch(error => {
-                console.error('Error:', error);
-              });
-        
-      }
-      const getDate = (time) => {
-        let d = new Date(time);
-        return d.toLocaleDateString()
-      }
-      const getTime = (time) => {
-        let d = new Date(time);
-        return d.toLocaleTimeString()
-      }
-      
+  //at the time of mounting
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`https://travelopia-3psd.onrender.com/booking?page=1`)
+      .then((res) => {
+        console.log(res);
+        setBookings(res.data.allBookings);
+        setTotalBookings(res.data.total);
+      })
+      .catch((err) => console.log(err));
+    setLoading(false);
+  }, []);
+
+  // to get the data on page increase and decrease
+  const getPagePosts = (p) => {
+    setCurrentPage(p);
+    setLoading(true);
+    axios
+      .get(`https://travelopia-3psd.onrender.com/booking?page=${p}`)
+      .then((res) => {
+        setLoading(false);
+        setBookings(res.data.allBookings);
+      })
+      .catch((err) => setLoading(false));
+  };
+
+  //for downloading the csv file
+  const downloadCSV = () => {
+    fetch(`https://travelopia-3psd.onrender.com/booking?page=${currentPage}`) // Replace with your API endpoint
+      .then((response) => response.text())
+      .then((csvData) => {
+        const blob = new Blob([csvData], { type: "text/csv;charset=utf-8" });
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "data.csv";
+        link.click();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  // for converting the date
+  const getDate = (time) => {
+    let d = new Date(time);
+    return d.toLocaleDateString();
+  };
+
+  // for converting the time
+  const getTime = (time) => {
+    let d = new Date(time);
+    return d.toLocaleTimeString();
+  };
+
   return (
     <>
-      <Text fontFamily={'Dancing Script'} fontSize={'6xl'} fontWeight={'bold'}>Travelopia</Text>
-        <Button colorScheme='whatsapp' onClick={()=>getPagePosts(currentPage)} rightIcon={<AiOutlineReload/>}>Reload</Button>
-       
-         <Table>
-            <Thead>
-                <Tr bgColor={'#BB86FC'} >
-                <Th color='black' fontSize={'sm'} border='1px solid black'>Booking Id</Th>
-                <Th color='black' fontSize={'sm'} border='1px solid black'>Booking Date</Th>
-                <Th color='black' fontSize={'sm'} border='1px solid black'>Booking Time</Th>
-                <Th color='black' fontSize={'sm'} border='1px solid black'>Name</Th>
-                <Th color='black' fontSize={'sm'} border='1px solid black'>Email</Th>
-                <Th color='black' fontSize={'sm'} border='1px solid black'>Destination</Th>
-                <Th color='black' fontSize={'sm'} border='1px solid black'>Total Travellers</Th>
-                <Th color='black' fontSize={'sm'} border='1px solid black'>Budget $</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-            {bookings.length > 0 && !loading ?bookings.map((booking)=>(
-            <Tr key={booking._id} border='1px solid black'>
+      <Text fontFamily={"Dancing Script"} fontSize={"6xl"} fontWeight={"bold"}>
+        Travelopia
+      </Text>
+      <Flex justifyContent={"space-between"}>
+        <Button
+          colorScheme="whatsapp"
+          onClick={() => getPagePosts(currentPage)}
+          rightIcon={<AiOutlineReload />}
+        >
+          Reload
+        </Button>
+        <Button
+          colorScheme="whatsapp"
+          onClick={() => navigate("/")}
+          rightIcon={<FaWpforms />}
+        >
+          Form
+        </Button>
+      </Flex>
+
+      <Table>
+        <Thead>
+          <Tr bgColor={"#BB86FC"}>
+            <Th color="black" fontSize={"sm"} border="1px solid black">
+              Booking Id
+            </Th>
+            <Th color="black" fontSize={"sm"} border="1px solid black">
+              Booking Date
+            </Th>
+            <Th color="black" fontSize={"sm"} border="1px solid black">
+              Booking Time
+            </Th>
+            <Th color="black" fontSize={"sm"} border="1px solid black">
+              Name
+            </Th>
+            <Th color="black" fontSize={"sm"} border="1px solid black">
+              Email
+            </Th>
+            <Th color="black" fontSize={"sm"} border="1px solid black">
+              Destination
+            </Th>
+            <Th color="black" fontSize={"sm"} border="1px solid black">
+              Total Travellers
+            </Th>
+            <Th color="black" fontSize={"sm"} border="1px solid black">
+              Budget $
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {bookings.length > 0 && !loading ? (
+            bookings.map((booking) => (
+              <Tr key={booking._id} border="1px solid black">
                 <Td>{booking._id}</Td>
                 <Td>{getDate(booking.createdAt)}</Td>
                 <Td>{getTime(booking.createdAt)}</Td>
@@ -91,25 +133,54 @@ const Dashboard = () => {
                 <Td>{booking.destination}</Td>
                 <Td>{booking.totalTravellers}</Td>
                 <Td>{booking.budget}</Td>
+              </Tr>
+            ))
+          ) : (
+            <Tr>
+              <Td colSpan={"8"}>
+                <Loader />
+              </Td>
             </Tr>
-        )) : <Tr>
-            <Td colSpan={'8'}><Loader/></Td>
-        </Tr> }
+          )}
         </Tbody>
-        </Table>
-        <Flex alignItems={'center'} marginInline={'auto'} border='1px solid lightgrey' justifyContent={'space-between'} padding='.5rem 1rem'>
-            <Button  colorScheme='orange'  onClick={downloadCSV}>Download CSV</Button>
-            <Flex alignItems={'center'} gap='1rem'>
-            <Button size={'sm'} colorScheme='orange' isDisabled={currentPage === 1} onClick={()=>getPagePosts(currentPage-1)}>Prev</Button>
-            <Text>{currentPage}/{Math.ceil(totalBooking.length/5)}</Text>
-            <Button size={'sm'} colorScheme='orange'  onClick={()=>getPagePosts(currentPage+1)} isDisabled={currentPage === Math.ceil(totalBooking.length/5)}>Next</Button>
-            </Flex>
-            <Box>Total Budget - {bookings.reduce((acc,el)=>el.budget+acc,0)}</Box>
+      </Table>
+      <Flex
+        alignItems={"center"}
+        marginInline={"auto"}
+        border="1px solid lightgrey"
+        justifyContent={"space-between"}
+        padding=".5rem 1rem"
+      >
+        <Button colorScheme="orange" onClick={downloadCSV}>
+          Download CSV
+        </Button>
+        <Flex alignItems={"center"} gap="1rem">
+          <Button
+            size={"sm"}
+            colorScheme="orange"
+            isDisabled={currentPage === 1}
+            onClick={() => getPagePosts(currentPage - 1)}
+          >
+            Prev
+          </Button>
+          <Text>
+            {currentPage}/{Math.ceil(totalBooking.length / 5)}
+          </Text>
+          <Button
+            size={"sm"}
+            colorScheme="orange"
+            onClick={() => getPagePosts(currentPage + 1)}
+            isDisabled={currentPage === Math.ceil(totalBooking.length / 5)}
+          >
+            Next
+          </Button>
         </Flex>
-      
-        
+        <Box>
+          Total Budget - {bookings.reduce((acc, el) => el.budget + acc, 0)}
+        </Box>
+      </Flex>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- frontend/src/Pages/FormPage.jsx
+++ frontend/src/Pages/FormPage.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
-import { FormControl, FormHelperText, FormLabel } from "@chakra-ui/form-control";
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+} from "@chakra-ui/form-control";
 import axios from "axios";
-import {BiUser} from 'react-icons/bi'
-import {BsCurrencyDollar, BsEnvelope} from 'react-icons/bs'
-import {FaUsers} from 'react-icons/fa'
+import { BiUser } from "react-icons/bi";
+import { BsCurrencyDollar, BsEnvelope } from "react-icons/bs";
+import { FaUsers } from "react-icons/fa";
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
 import "./styles/FormPage.css";
 import { Select } from "@chakra-ui/select";
 import Loader from "../Components/Loader";
 import { Button } from "@chakra-ui/button";
 import { useNavigate } from "react-router";
-import { Text } from "@chakra-ui/layout";
+import { Box, Text } from "@chakra-ui/layout";
 import { useToast } from "@chakra-ui/toast";
 
 const FormPage = () => {
@@ -38,117 +42,151 @@ const FormPage = () => {
       .then((res) => {
         setLoading(false);
         return toast({
-            title: 'Booking Successfull.',
+          title: "Booking Successfull.",
           description: "Go to Dashboard to see all Bookings.",
-          status: 'success',
+          status: "success",
           duration: 5000,
-          position:"top",
+          position: "top",
           isClosable: true,
-        })
+        });
       })
       .catch((err) => {
         setLoading(false);
         return toast({
-            title: 'An Error Occured.',
-          description: "Something went wrong. Please try again after some time !",
-          status: 'error',
+          title: "An Error Occured.",
+          description:
+            "Something went wrong. Please try again after some time !",
+          status: "error",
           duration: 5000,
-          position:"top",
+          position: "top",
           isClosable: true,
-        })
+        });
       });
   };
 
- 
   return (
     <>
-      <Text fontFamily={'Dancing Script'} fontSize={'6xl'} fontWeight={'bold'}>Travelopia</Text>
-    <Button data-testid='dashboard' colorScheme="twitter" onClick={()=>navigate("/dashboard")} position='absolute' top='10' right='10'>Dashboard</Button>
+      <Text fontFamily={"Dancing Script"} fontSize={"6xl"} fontWeight={"bold"}>
+        Travelopia
+      </Text>
+      <Button
+        data-testid="dashboard"
+        colorScheme="twitter"
+        onClick={() => navigate("/dashboard")}
+        position="absolute"
+        top="10"
+        right="10"
+      >
+        Dashboard
+      </Button>
+      <Box
+        color="white"
+        fontFamily={"Dancing Script"}
+        marginInline={"auto"}
+        padding={".5rem .7rem"}
+        borderRadius={".3rem"}
+        bgColor={"red"}
+        w="fit-content"
+        fontSize={"2xl"}
+      >
+        Submit Form to Book your excited Journey !
+      </Box>
       <form onSubmit={handleFormSubmit}>
-       {loading ? <Loader/>: <FormControl>
-          <FormLabel>Name</FormLabel>
-          <InputGroup>
-          <InputLeftElement>
-          <BiUser/>
-          </InputLeftElement>
-          <Input
-            name="name"
-            type="text"
-            data-testid='name'
-            isRequired
-            placeholder="Enter your name"
-            onChange={handleFormChange}
-          />
-          </InputGroup>
-
-          <FormLabel>Email</FormLabel>
-          <InputGroup>
-          <InputLeftElement>
-          <BsEnvelope/>
-          </InputLeftElement>
-          <Input
-            type="email"
-            name="email"
-            isRequired
-            placeholder="Enter your Email"
-            data-testid='email'
-            onChange={handleFormChange}
-            />
+        {loading ? (
+          <Loader />
+        ) : (
+          <FormControl>
+            <FormLabel>Name</FormLabel>
+            <InputGroup>
+              <InputLeftElement>
+                <BiUser />
+              </InputLeftElement>
+              <Input
+                name="name"
+                type="text"
+                data-testid="name"
+                isRequired
+                placeholder="Enter your name"
+                onChange={handleFormChange}
+              />
             </InputGroup>
 
-          <FormLabel>Destination</FormLabel>
-          
-              <Select
-            placeholder="Select Destination"
-            data-testid='destination'
-            
-            onChange={handleFormChange}
-            name="destination"
+            <FormLabel>Email</FormLabel>
+            <InputGroup>
+              <InputLeftElement>
+                <BsEnvelope />
+              </InputLeftElement>
+              <Input
+                type="email"
+                name="email"
+                isRequired
+                placeholder="Enter your Email"
+                data-testid="email"
+                onChange={handleFormChange}
+              />
+            </InputGroup>
+
+            <FormLabel>Destination</FormLabel>
+
+            <Select
+              placeholder="Select Destination"
+              data-testid="destination"
+              onChange={handleFormChange}
+              name="destination"
             >
-            <option value="India">India</option>
-            <option value="Africa">Africa</option>
-            <option value="Europe">Europe</option>
-          </Select>
+              <option value="India">India</option>
+              <option value="Africa">Africa</option>
+              <option value="Europe">Europe</option>
+            </Select>
 
-          <FormLabel>Number of Travellers</FormLabel>
-          <InputGroup>
-          <InputLeftElement>
-          <FaUsers/>
-          </InputLeftElement>
-          <Input
-            type="number"
-            data-testid='totalTravellers'
-            isRequired
-            placeholder="Total Travellers"
-            onChange={handleFormChange}
-            name="totalTravellers"
-            />
+            <FormLabel>Number of Travellers</FormLabel>
+            <InputGroup>
+              <InputLeftElement>
+                <FaUsers />
+              </InputLeftElement>
+              <Input
+                type="number"
+                data-testid="totalTravellers"
+                isRequired
+                placeholder="Total Travellers"
+                onChange={handleFormChange}
+                name="totalTravellers"
+              />
             </InputGroup>
-          <FormLabel>Budget $</FormLabel>
-          <InputGroup>
-          <InputLeftElement>
-          <BsCurrencyDollar/>
-          </InputLeftElement>
-          <Input
-            name="budget"
-            type="number"
-            data-testid = 'budget'
-            isRequired
-            placeholder="Budget Per Person (in dollars $)"
-            onChange={handleFormChange}
-            />
+            <FormLabel>Budget $</FormLabel>
+            <InputGroup>
+              <InputLeftElement>
+                <BsCurrencyDollar />
+              </InputLeftElement>
+              <Input
+                name="budget"
+                type="number"
+                data-testid="budget"
+                isRequired
+                placeholder="Budget Per Person (in dollars $)"
+                onChange={handleFormChange}
+              />
             </InputGroup>
-            <FormHelperText color='white' border='1px solid white' w='fit-content' padding={'.3rem'} borderRadius={'5px'}>Net Budget {form.budget*form.totalTravellers}</FormHelperText>
-                
-          <Input
-            type="submit"
-            value="Submit"
-            bgColor="#26cc00"
-            color='white'
-            mt="1rem"
-            w="30%"
-          />
-        </FormControl>}
+            <FormHelperText
+              color="white"
+              border="1px solid white"
+              w="fit-content"
+              padding={".3rem"}
+              borderRadius={"5px"}
+            >
+              Net Budget {form.budget * form.totalTravellers}
+            </FormHelperText>
+
+            <Input
+              type="submit"
+              value="Submit"
+              bgColor="#26cc00"
+              color="white"
+              mt="1rem"
+              w="30%"
+            />
+          </FormControl>
+        )}
       </form>
     </>
   );
